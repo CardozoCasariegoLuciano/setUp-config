2faf2fa9b15f9b953a62faefbe8b4e2807da04cb
Luciano Cardozo Casariego <lucianoc.c117@gmail.com>
2025-05-27 20:18:41 -0300

    add config to rest-nvim plugin

--- 20-4-2025/nvim/lua/plugins/rest-nvim.lua
+++ 20-4-2025/nvim/lua/plugins/rest-nvim.lua
@@ -1,26 +1,34 @@
 ---@diagnostic disable: undefined-global
 return {
-    "rest-nvim/rest.nvim",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        opts = function(_, opts)
-            opts.ensure_installed = opts.ensure_installed or {}
-            table.insert(opts.ensure_installed, "http")
-        end,
-    },
-    config = function()
-        require("rest-nvim").setup({
-            -- Opcional: Define un formato personalizado para archivos
-            custom_files = {
-                "*.http",
-                "*.rest",
-            },
-            -- Configura el comportamiento de las peticiones
-            jump_to_request = false, -- Evita saltar al resultado automáticamente
-        })
+  "rest-nvim/rest.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      table.insert(opts.ensure_installed, "http")
+    end,
+  },
+  config = function()
+    require("rest-nvim").setup({
+      -- Opcional: Define un formato personalizado para archivos
+      custom_files = {
+        "*.http",
+        "*.rest",
+      },
+      -- Configura el comportamiento de las peticiones
+      jump_to_request = false, -- Evita saltar al resultado automáticamente
+    })
 
-        local key = vim.keymap                                   -- for keymaps
-        key.set('n', '<Leader>xr', ":vertical Rest run<CR>", {}) --Find file by name
-    end
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "json",
+      callback = function(ev)
+        vim.bo[ev.buf].formatprg = "jq"
+        print("It's a json file")
+      end,
+    })
+
+    local key = vim.keymap                                   -- for keymaps
+    key.set('n', '<Leader>xr', ":vertical Rest run<CR>", {}) --Find file by name
+  end
 
 }
